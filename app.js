f5e9ddbfc1161b4d934aed2a0471848098d23b17
ToushiroHotoru <toushirohotoru@gmail.com>
2021-11-14 17:23:38 +0300

    Настроил модель бд

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const DataForm = require("./models/dataForms");
 
 const app = express();
 
@@ -10,20 +11,53 @@ app.use(express.static("public"));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
+});
+
+app.get("/send-data", async (req, res) => {
+    const dataForm = new DataForm({
+        city: {
+            cityName: 'cheby',
+            company: [{
+                companyName: 'fist.inc',
+                workers: [{
+                    workerName: 'Tolyan',
+                }],
+            }],
+        }
+    });
+    try {
+        const data = await dataForm.save();
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get("/all-data", async (req, res) => {
+    try {
+        const data = await DataForm.find()
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+    res.render("about");
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+    res.status(404).render("404");
 });
 
-const dbURI =
-  "mongodb+srv://toushiro:asagilove@cluster0.ssuiv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(5000))
-  .catch((err) => console.log(err));
+const dbURI = "mongodb+srv://toushiro:asagilove@cluster0.ssuiv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((result) => {
+        app.listen(5000)
+        console.log("app is working")
+    })
+    .catch((err) => console.log(err));
---
+++ models/dataForms.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dataForms = new Schema({
+  city: {
+    cityName: {
+      type: String,
+      required: true
+    },
+    company: [{
+      companyName: {
+        type: String,
+      },
+      workers: [{
+        workerName: {
+          type: String,
+        },
+      }],
+    }],
+  }
+}, {
+  timestamps: true
+});
+
+const dataFrom = mongoose.model("dataFrom", dataForms);
+module.exports = dataFrom;
